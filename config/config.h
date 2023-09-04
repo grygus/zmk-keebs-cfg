630abd0bfbc41c4ac577aaec4f326e1b93c28e44
Dominik Zmitrowicz <dogrizz@gmail.com>
2023-09-05 01:07:04 +0200

    Remove mouse and more work on totem

--- config/config.h
+++ config/config.h
@@ -1,6 +1,5 @@
 #define AS(keycode) &as LS(keycode) keycode
 #define TAP_MS 200
-#define U_MOUSE_MOVE_MAX 1500
 
 #define LOWER 1
 #define RAISED 2
@@ -18,18 +17,3 @@
   tapping-term-ms = <TAP_MS>;
   quick-tap-ms = <TAP_MS>;
 };
-
-&mmv {
-  acceleration-exponent = <1>;
-  time-to-max-speed-ms = <1000>;
-  delay-ms = <0>;                   
-};
-
-#undef MOVE_UP
-#undef MOVE_DOWN
-#undef MOVE_LEFT
-#undef MOVE_RIGHT
-#define MOVE_UP MOVE_VERT(-U_MOUSE_MOVE_MAX)
-#define MOVE_DOWN MOVE_VERT(U_MOUSE_MOVE_MAX)
-#define MOVE_LEFT MOVE_HOR(-U_MOUSE_MOVE_MAX)
-#define MOVE_RIGHT MOVE_HOR(U_MOUSE_MOVE_MAX)
