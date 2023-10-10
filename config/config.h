e6567963d18ec6d3c5ad724e24d686eb06db6895
Dominik Zmitrowicz <dogrizz@gmail.com>
2023-10-10 20:03:12 +0200

    try to balance mod taps

--- config/config.h
+++ config/config.h
@@ -5,8 +5,15 @@
 #define RAISED 2
 #define ADJUST 3
 
-&mt {
+&ht {
   flavor = "tap-preferred";
+  tapping-term-ms = <TAP_MS>;
+  quick-tap-ms = <TAP_MS>;
+  hold-trigger-key-positions = <0>;
+};
+
+&mt {
+  flavor = "balanced";
   tapping-term-ms = <TAP_MS>;
   quick-tap-ms = <TAP_MS>;
   hold-trigger-key-positions = <0>;
