fcdfb8ac05260867e964f2b7d593d5171c62bccc
Dominik Zmitrowicz <dogrizz@gmail.com>
2024-09-23 23:39:52 +0200

    Try to rework layer to make ADJUST always the top one

--- config/config.h
+++ config/config.h
@@ -1,13 +1,9 @@
 #define AS(keycode) &as LS(keycode) keycode
 #define TAP_MS 200
 #define QUICK_TAP_MS 0
-
 #define LOWER 1
 #define RAISED 2
-#define ADJUST 3
-#define D_LOWER 5
-#define D_RAISED 6
-#
+
 &mt {
   flavor = "tap-preferred";
   tapping-term-ms = <TAP_MS>;
